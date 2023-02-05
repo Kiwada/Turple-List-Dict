5f4cbc44612180704271ca847932f498ed674a5c
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-02-05 16:53:41 -0300

    Add files via upload

---
+++ #Dicionarios Ex.py
@@ -0,0 +1,10 @@
+#Dicionarios Ex
+
+Pessoas = { ' nome ' : ' Gustavo' ,' sexo' :'M' ,' idade' : 22}
+#print(f'O {Pessoas[" nome "]} tem {Pessoas [" idade"]} anos')
+#print(Pessoas.keys())
+#print(Pessoas.items())
+
+for k ,v in Pessoas.items():
+    print(f'{k} :D {v}')
+    
