cf53b32673c5557c19e5352931ea92565eab57d7
Kaio Fontenele <82303943+Kiwada@users.noreply.github.com>
2023-02-05 17:08:44 -0300

    Add files via upload

---
+++ dicionario uf.py
@@ -0,0 +1,7 @@
+brasil = []
+estado1 = {'uf' : 'Rio De Janeiro', 'Sigla' : 'RJ'}
+estado2 = {'uf' : 'Píaui' , 'Sigla' : 'PI'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[1] ['Sigla']) 
